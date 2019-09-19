7639a1d8367cc665276308c7b1f561f2fb1da15f
Michael Poole <mpoole4@comcast.net>
2019-09-19 13:22:39 -0500

    Initial commit

---
+++ Assets/External/Animated/DoorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public bool isLocked;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(isLocked == false)
+        {
+            anim.SetTrigger("OpenDoor");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+    }
+
+    void PauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
