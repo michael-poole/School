1ece5e2c06cb9223966afc8c97e1443c2acf9a16
Poole <mgp188@iastate.edu>
2019-12-10 16:01:45 -0600

    Added in HTC Vive functionality

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float Speed;
+
+    private Camera Camera;
+
+    void Start()
+    {
+        Camera = transform.GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        if (SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport").GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            transform.Translate(Vector3.Scale(Camera.transform.forward, new Vector3(Speed * Time.deltaTime, 0, Speed * Time.deltaTime)));
+            print("Got it");
+        }
+    }
+}
